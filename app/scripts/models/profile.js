7ecdf3af5b672ed03c72c96bb2e2cbcfdfdbe1a0
Jack Flannery <jmflannery81@gmail.com>
2014-01-04 15:10:37 -0500

    update the profile template and some convienience methods on ProfileModel

--- app/scripts/models/profile.js
+++ app/scripts/models/profile.js
@@ -27,6 +27,14 @@ define([
 
     fullName: function() {
       return this.get('firstname') + ' ' + this.get('middlename') + ' ' + this.get('lastname');
+    },
+
+    fullNickName: function() {
+      return this.get('nickname') + ' ' + this.get('lastname');
+    },
+
+    fullNickNameWithSuffix: function() {
+      return this.get('nickname') + ' ' + this.get('lastname') + ' ' + this.get('suffix');
     }
   });
 
